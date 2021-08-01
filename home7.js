f45b52488d4595053a0532287b60fe4297d1702d
NataSereda <natalya.romanyak@gmail.com>
2021-08-01 17:23:37 +0300

    12

--- home7.js
+++ home7.js
@@ -2,11 +2,10 @@ function goUp(up, cb) {
     console.log('Будильник')
     setTimeout(() => {
         if (up) {
-            console.log('Вставай!');
+            console.log('Пора вставати');
             cb(null,"Я вже встала");
         } else {
             cb('Ще трішки хочеться спати');
         }
     }, 600)
 }
-fu
